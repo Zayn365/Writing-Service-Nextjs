80d2c8a1e29e68175e8046f38f321b98c21bb748
Zayn365 <zaynsaeed365@gmail.com>
2024-07-09 00:48:12 -0700

    hot fixed

--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -1,49 +1,42 @@
 import Image from "next/image";
-import {
-  Typography,
-  Card,
-  CardBody,
-} from "@material-tailwind/react";
-
+import { Typography, Card, CardBody } from "@material-tailwind/react";
 
 interface ArticleCardProps {
-    img: string;
-    title: string;
-    desc: string;
-  }
-  
-  export function ArticleCard({ img, title, desc }: ArticleCardProps) {
-    return (
-      // @ts-ignore 
-      <Card
-        className="relative grid min-h-[30rem] items-end overflow-hidden rounded-xl"
-        color="transparent"
-      >
-        <Image
-          width={768}
-          height={768}
-          src={img}
-          alt="bg"
-          className="absolute inset-0 h-full w-full object-cover object-center"
-        />
-        <div className="absolute inset-0 bg-black/60" />
-        {/* @ts-ignore */}
-        <CardBody className="relative flex flex-col justify-end">
-          {/* @ts-ignore */}
-          <Typography variant="h4" color="white">
-            {title}
-          </Typography>
-          {/* @ts-ignore */}
-          <Typography
-            variant="paragraph"
-            color="white"
-            className="my-2 font-normal"
-          >
-            {desc}
-          </Typography>
-        </CardBody>
-      </Card>
-    );
-  }
+  img: string;
+  title: string;
+  desc: string;
+}
 
-  export default ArticleCard;
+export function ArticleCard({ img, title, desc }: ArticleCardProps) {
+  return (
+    // @ts-ignore
+    <Card
+      className="relative grid min-h-[30rem] items-end overflow-hidden rounded-xl"
+      color="transparent">
+      <Image
+        width={768}
+        height={768}
+        src={img}
+        alt="bg"
+        className="absolute inset-0 h-full w-full object-cover object-center"
+      />
+      <div className="absolute inset-0 bg-black/60" />
+      {/* @ts-ignore */}
+      <CardBody className="relative flex flex-col justify-end">
+        {/* @ts-ignore */}
+        <Typography variant="h4" color="white">
+          {title}
+        </Typography>
+        {/* @ts-ignore */}
+        <Typography
+          variant="paragraph"
+          color="white"
+          className="my-2 font-normal">
+          {desc}
+        </Typography>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default ArticleCard;
