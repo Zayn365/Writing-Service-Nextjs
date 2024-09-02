82b6c32dbda300b1377b449e3917b7a239d93b0d
Zayn365 <zaynsaeed365@gmail.com>
2024-09-02 23:01:34 +0500

    fixed typo

--- src/hooks/EditData.tsx
+++ src/hooks/EditData.tsx
@@ -1,11 +1,11 @@
 import { Axios } from '@/utils/Axios';
 import { toast } from 'react-toastify';
 import axios from "axios";
-const localurl = "https://write-articles-for-me.vercel.app/";
+const BASE_URL = "https://write-articles-for-me.vercel.app/";
 
 export const EditData = async (Id: string, endpoint: string, data: any) => {
     try {
-        const response = await axios.put(localurl + `/api/${endpoint}?id=${Id}`, data);
+        const response = await Axios.put(`${BASE_URL}/api/${endpoint}?id=${Id}`, data);
         toast.success("upated!!")
         if (response.status !== 200) {
             throw new Error(`Error: ${response.statusText}`);
